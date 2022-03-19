2a6a44f25e18438cf46e45b0a41d32d3b907d860
AktamNosirov <94_aktam@mail.ru>
2022-03-19 09:14:37 +0300

    17

--- logic17.py
+++ logic17.py
@@ -6,4 +6,9 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x1=a%10
+    x2=a//10
+    x3=a//100
+    x4=a//1000
+    x5=a//10000
+    return bool(1) if x1<x2<x3<x4<x5 else bool(0)
