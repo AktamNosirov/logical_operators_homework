368b0cc89ab1f7561cd6355bb88472cba87bb291
AktamNosirov <94_aktam@mail.ru>
2022-03-21 10:03:58 +0300

    20

--- logic20.py
+++ logic20.py
@@ -15,19 +15,16 @@ def main(n):
     x5=n%100000//10000
     number_of_ones=0 
     number_of_zero=0
-    if x1-1==0 and 0<n<100000: number_of_ones+=1
-    if x1-1==-1 and 0<n<100000 : number_of_zero+=1 
-    if x2-1==0 and 0<n<100000 : number_of_ones+=1
-    if x2-1==-1 and 0<n<100000 : number_of_zero+=1 
-    if x3-1==0 and 0<n<100000 : number_of_ones+=1
-    if x3-1==-1 and 0<n<100000 : number_of_zero+=1 
-    if x4-1==0 and 0<n<100000 : number_of_ones+=1
-    if x4-1==-1 and 0<n<100000 : number_of_zero+=1 
-    if x5-1==0 and 0<n<100000 : number_of_ones+=1
-    if x5-1==-1 and 0<n<100000 : number_of_zero+=1 
+    if x1==1: number_of_ones+=1
+    if x1==0 : number_of_zero+=1 
+    if x2==1: number_of_ones+=1
+    if x2==0: number_of_zero+=1 
+    if x3==1 : number_of_ones+=1
+    if x3==0  : number_of_zero+=1 
+    if x4==1 : number_of_ones+=1
+    if x4==0 : number_of_zero+=1 
+    if x5==1 : number_of_ones+=1
+    if x5==0: number_of_zero+=1 
     if number_of_ones>number_of_zero :return bool(1) 
-    else : return bool(0)
-   
-
-
-print(main(10001))
+    else : return bool (0)
+print(main(1001))
