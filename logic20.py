2085a166254a6045b55521de5ccffcc5fed9c41d
AktamNosirov <94_aktam@mail.ru>
2022-03-21 09:15:41 +0300

    20.1

--- logic20.py
+++ logic20.py
@@ -8,5 +8,28 @@ def main(n):
     Returns:
         bool: answer
     """
+    x1=n%10
+    x2=n%100//10
+    x3=n%1000//100
+    x4=n%10000//1000
+    x5=n%100000//10000
+    x6=n%1000000//100000
+    number_of_ones=0 
+    number_of_zero=0
+    if x1-1==0 and 1<=n<=100000: number_of_ones+=1
+    if x1-1==-1 and 1<=n<=100000 : number_of_zero+=1 
+    if x2-1==0 and 1<=n<=100000 : number_of_ones+=1
+    if x2-1==-1 and 1<=n<=100000 : number_of_zero+=1 
+    if x3-1==0 and 1<=n<=100000 : number_of_ones+=1
+    if x3-1==-1 and 1<=n<=100000 : number_of_zero+=1 
+    if x4-1==0 and 1<=n<=100000 : number_of_ones+=1
+    if x4-1==-1 and 1<=n<=100000 : number_of_zero+=1 
+    if x5-1==0 and 1<=n<=100000 : number_of_ones+=1
+    if x5-1==-1 and 1<=n<=100000 : number_of_zero+=1 
+    if x6-1==0 and 1<=n<=100000 : number_of_ones+=1
+    if x6-1==-1 and 1<=n<=100000 : number_of_zero+=1 
+    if number_of_ones>number_of_zero :return bool(1) 
+    if number_of_ones<number_of_zero : return bool(0) 
 
-    return
+
+print(main(10001))
