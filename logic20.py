1bb2c6984cd460f46e09960e64cd484f2bbb4d9c
AktamNosirov <94_aktam@mail.ru>
2022-03-21 09:18:15 +0300

    20.2

--- logic20.py
+++ logic20.py
@@ -16,20 +16,20 @@ def main(n):
     x6=n%1000000//100000
     number_of_ones=0 
     number_of_zero=0
-    if x1-1==0 and 1<=n<=100000: number_of_ones+=1
-    if x1-1==-1 and 1<=n<=100000 : number_of_zero+=1 
-    if x2-1==0 and 1<=n<=100000 : number_of_ones+=1
-    if x2-1==-1 and 1<=n<=100000 : number_of_zero+=1 
-    if x3-1==0 and 1<=n<=100000 : number_of_ones+=1
-    if x3-1==-1 and 1<=n<=100000 : number_of_zero+=1 
-    if x4-1==0 and 1<=n<=100000 : number_of_ones+=1
-    if x4-1==-1 and 1<=n<=100000 : number_of_zero+=1 
-    if x5-1==0 and 1<=n<=100000 : number_of_ones+=1
-    if x5-1==-1 and 1<=n<=100000 : number_of_zero+=1 
-    if x6-1==0 and 1<=n<=100000 : number_of_ones+=1
-    if x6-1==-1 and 1<=n<=100000 : number_of_zero+=1 
+    if x1-1==0 and 0<n<100000: number_of_ones+=1
+    if x1-1==-1 and 0<n<100000 : number_of_zero+=1 
+    if x2-1==0 and 0<n<100000 : number_of_ones+=1
+    if x2-1==-1 and 0<n<100000 : number_of_zero+=1 
+    if x3-1==0 and 0<n<100000 : number_of_ones+=1
+    if x3-1==-1 and 0<n<100000 : number_of_zero+=1 
+    if x4-1==0 and 0<n<100000 : number_of_ones+=1
+    if x4-1==-1 and 0<n<100000 : number_of_zero+=1 
+    if x5-1==0 and 0<n<100000 : number_of_ones+=1
+    if x5-1==-1 and 0<n<100000 : number_of_zero+=1 
+    if x6-1==0 and 0<n<100000 : number_of_ones+=1
+    if x6-1==-1 and 0<n<100000 : number_of_zero+=1 
     if number_of_ones>number_of_zero :return bool(1) 
     if number_of_ones<number_of_zero : return bool(0) 
 
 
-print(main(10001))
+print(main(1001))
